adb03993facd473625ebececad59812a05caf5c2
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2016-06-22 15:32:01 -0400

    First RobotControl stub

---
+++ RobotControl/Geometry.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RobotControl
+{
+
+    //██████╗  ██████╗ ██╗███╗   ██╗████████╗
+    //██╔══██╗██╔═══██╗██║████╗  ██║╚══██╔══╝
+    //██████╔╝██║   ██║██║██╔██╗ ██║   ██║   
+    //██╔═══╝ ██║   ██║██║██║╚██╗██║   ██║   
+    //██║     ╚██████╔╝██║██║ ╚████║   ██║   
+    //╚═╝      ╚═════╝ ╚═╝╚═╝  ╚═══╝   ╚═╝                                  
+    /// <summary>
+    /// Represents three coordinates in space.
+    /// </summary>
+    public class Point
+    {
+        public double x, y, z;
+
+        public Point(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return "[" + this.x + "," + this.y + "," + this.z + "]";
+        }
+    }
+
+
+    //██████╗  ██████╗ ████████╗ █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+    //██╔══██╗██╔═══██╗╚══██╔══╝██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+    //██████╔╝██║   ██║   ██║   ███████║   ██║   ██║██║   ██║██╔██╗ ██║
+    //██╔══██╗██║   ██║   ██║   ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+    //██║  ██║╚██████╔╝   ██║   ██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+    //╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+    /// <summary>
+    /// Represents a rotation using quaternions.
+    /// </summary>
+    public class Rotation
+    {
+        /// <summary>
+        /// The orientation of a global XYZ coordinate system.
+        /// </summary>
+        public static readonly Rotation GlobalXY = new Rotation(1, 0, 0, 0);
+
+        /// <summary>
+        /// A global XYZ coordinate system rotated 180 degs around its X axis.
+        /// </summary>
+        public static readonly Rotation FlippedAroundX = new Rotation(0, 1, 0, 0);
+
+        /// <summary>
+        /// A global XYZ coordinate system rotated 180 degs around its Y axis. 
+        /// Recommended as the easiest orientation for the standard robot end effector to reach in positive X octants.
+        /// </summary>
+        public static readonly Rotation FlippedAroundY = new Rotation(0, 0, 1, 0);
+
+        /// <summary>
+        /// A global XYZ coordinate system rotated 180 degs around its Z axis.
+        /// </summary>
+        public static readonly Rotation FlippedAroundZ = new Rotation(0, 0, 0, 1);
+
+        public double q1, q2, q3, q4;
+
+        public Rotation(double q1, double q2, double q3, double q4)
+        {
+            this.q1 = q1;
+            this.q2 = q2;
+            this.q3 = q3;
+            this.q4 = q4;
+        }
+
+        //// From Tait-Bryan angles (rations around X, Y, Z axes)
+        // // See here: http://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToQuaternion/
+        //public Rotation (double roll, double pitch, double yaw)
+        //{
+
+        //}
+
+        public override string ToString()
+        {
+            return "[" + this.q1 + "," + this.q2 + "," + this.q3 + "," + this.q4 + "]";
+        }
+    }
+
+
+
+    //███████╗██████╗  █████╗ ███╗   ███╗███████╗
+    //██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝
+    //█████╗  ██████╔╝███████║██╔████╔██║█████╗  
+    //██╔══╝  ██╔══██╗██╔══██║██║╚██╔╝██║██╔══╝  
+    //██║     ██║  ██║██║  ██║██║ ╚═╝ ██║███████╗
+    //╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝                                      
+    /// <summary>
+    /// Represents a location and rotation in 3D space
+    /// </summary>
+    public class Frame
+    {
+        /// <summary>
+        /// This is the default rotation that will be assigned to Frames constructed only with location properties.
+        /// </summary>
+        public static Rotation DefaultOrientation = Rotation.FlippedAroundY;
+
+        public Point position;
+        public Rotation orientation;
+
+        public Frame(double x, double y, double z)
+        {
+            this.position = new Point(x, y, z);
+            this.orientation = DefaultOrientation;
+        }
+
+        public Frame(double x, double y, double z, double q1, double q2, double q3, double q4)
+        {
+            this.position = new Point(x, y, z);
+            this.orientation = new Rotation(q1, q2, q3, q4);
+        }
+
+        public Frame(Point position)
+        {
+            this.position = position;
+            this.orientation = DefaultOrientation;
+        }
+
+        public Frame(Point position, Rotation orientation)
+        {
+            this.position = position;
+            this.orientation = orientation;
+        }
+
+        public override string ToString()
+        {
+            return this.position + "," + this.orientation;
+        }
+    }
+
+
+
+    //██████╗  █████╗ ████████╗██╗  ██╗
+    //██╔══██╗██╔══██╗╚══██╔══╝██║  ██║
+    //██████╔╝███████║   ██║   ███████║
+    //██╔═══╝ ██╔══██║   ██║   ██╔══██║
+    //██║     ██║  ██║   ██║   ██║  ██║
+    //╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
+    /// <summary>
+    /// Represents an ordered sequence of target Frames
+    /// </summary>
+    public class Path
+    {
+        private List<Frame> targets;
+        public int targetCount { get; private set; }
+
+        public Path()
+        {
+            this.targets = new List<Frame>();
+            targetCount = 0;
+        }
+
+        public void Add(Frame target)
+        {
+            this.targets.Add(target);
+            targetCount++;
+        }
+
+        public void Add(Point position)
+        {
+            this.Add(new Frame(position));
+        }
+
+        public void Add(double x, double y, double z)
+        {
+            this.Add(new Frame(x, y, z));
+        }
+
+        public void Add(Point position, Rotation orientation)
+        {
+            this.Add(new Frame(position, orientation));
+        }
+
+        public Frame GetTarget(int index)
+        {
+            return this.targets[index];
+        }
+
+    }
+
+}
---
+++ RobotControl/RAPID.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RobotControl
+{
+    internal class RAPID
+    {
+
+        /// <summary>
+        /// Given a Path, and constant velocity and zone for all targets, returns a string representation of a RAPID module. Velocity and zone must comply with predefined types.
+        /// WARNING: this method is EXTREMELY UNSAFE, since it performs no IK calculations, assuming all targets are in the positive XYZ octant hence a robot configuration of [0,0,0,0]. To be extended with a proper module creator
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <param name="velocity"></param>
+        /// <param name="zone"></param>
+        /// <returns></returns>
+        public static List<string> UNSAFEModuleFromPath(string name, Path path, int velocity, int zone)
+        {
+            string vel = "v" + velocity;
+            string zon = "z" + zone;
+
+            List<string> module = new List<string>();
+
+            module.Add("MODULE " + name);
+            module.Add("");
+
+            for (int i = 0; i < path.targetCount; i++)
+            {
+                Frame t = path.GetTarget(i);
+                module.Add("  CONST robtarget Target_" + i
+                    + ":=" + UNSAFEExplicitRobTargetDeclaration(path.GetTarget(i)) + ";");
+            }
+
+            module.Add("");
+            module.Add("  PROC main()");
+            module.Add(@"    ConfJ \Off;");
+            module.Add(@"    ConfL \Off;");
+
+            for (int i = 0; i < path.targetCount; i++)
+            {
+                module.Add("    MoveL Target_" + i
+                    + "," + vel
+                    + "," + zon
+                    + @",Tool0\WObj:=WObj0;");
+            }
+
+            module.Add("  ENDPROC");
+            module.Add("ENDMODULE");
+
+            return module;
+        }
+
+        /// <summary>
+        /// Returns a quick and dirty RobTarget declaration out of a Frame object.
+        /// WARNING: this method is extremely unsafe! It assumes the target is in the positive XYZ octant 
+        /// and assigns a [0,0,0,0] robot configuration to the target. Also, it performs no FK/IK nor security checks.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static string UNSAFEExplicitRobTargetDeclaration(Frame target)
+        {
+            return "[" + target + ",[0,0,0,0],[0,9E9,9E9,9E9,9E9,9E9]]";
+        }
+
+
+    }
+}
