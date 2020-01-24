6ae5b374a0a9bd136ee9c309fff9b8a597dd6939
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2020-01-24 15:29:13 -0500

    Add `ParseableFromString` attribute

---
+++ src/Machina/Attributes/ParseableFromString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machina.Attributes
+{
+    /// <summary>
+    /// A `ParseableFromString` API method is one that can be invoked through reflection
+    /// by parsing a string representation of a call to such method. 
+    /// This attribute is used to flag those methods that can be called by typing the
+    /// action with plain text, like the Bridge Console or a string from Grasshopper. 
+    /// </summary>
+    public class ParseableFromString : Attribute
+    {
+    }
+}
