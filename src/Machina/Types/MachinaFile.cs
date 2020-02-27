bbef493687f374d7d62aa82bc7e3bcd93ff76a5a
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2020-02-27 10:52:27 -0500

    Serialization of `MachinaFile`

--- src/Machina/Types/MachinaFile.cs
+++ src/Machina/Types/MachinaFile.cs
@@ -16,20 +16,46 @@ namespace Machina.Types
         internal string Name { get; }
         internal string Extension { get; }
         internal Encoding Encoding { get; }
+        internal char CommentChar { get; }
 
         internal List<string> Content { get; private set; }
 
-        internal MachinaFile(string name, string extension, Encoding encoding)
+        internal MachinaFile(string name, string extension, Encoding encoding, char commentChar)
         {
             this.Name = name;
             this.Extension = extension;
             this.Encoding = encoding;
+            this.CommentChar = commentChar;
         }
 
         internal void SetContent(List<string> lines)
         {
             this.Content = lines;
         }
+         
+        public override string ToString()
+        {
+            return String.Join(Environment.NewLine, ToStringList());
+        }
 
+        internal List<string> ToStringList()
+        {
+            List<string> lines = new List<string>();
+            lines.AddRange(GetHeader());
+            lines.AddRange(Content);
+            lines.Add("");
+            return lines;
+        }
+
+        private List<string> GetHeader()
+        {
+            List<string> header = new List<string>();
+            string ccline = new String(CommentChar, 80);
+            header.Add(ccline);
+            header.Add($"{CommentChar}{CommentChar} FILENAME: \"{Name}.{Extension}\"");
+            header.Add(ccline);
+            header.Add("");
+            return header;
+        }
     }
 }
