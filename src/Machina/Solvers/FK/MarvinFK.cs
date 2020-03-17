b574e594d78dda121c262d94afd4149e1aa9c18a
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2020-03-16 20:37:32 -0400

    Code cleanup

--- src/Machina/Solvers/FK/MarvinFK.cs
+++ src/Machina/Solvers/FK/MarvinFK.cs
@@ -54,6 +54,13 @@ namespace Machina.Solvers.FK
             _model = (RobotSixAxesArm) model;  // we want an exception if this doesn't work...
         }
 
+
+        /// <summary>
+        /// Compute the FK of this robot for the end-effector. 
+        /// </summary>
+        /// <param name="jointValues"></param>
+        /// <param name="units"></param>
+        /// <returns></returns>
         internal override List<Matrix> ForwardKinematics(List<double> jointValues, Units units)
         {
             // Sanity
@@ -63,23 +70,22 @@ namespace Machina.Solvers.FK
             }
 
             // Convert to radians
-            List<double> rotsRad = new List<double>();
+            List<double> rots = new List<double>();
             if (units == Units.Degrees)
             {
                 for (int i = 0; i < jointValues.Count; i++)
                 {
-                    rotsRad.Add(jointValues[i] * MMath.TO_RADS);
+                    rots.Add(jointValues[i] * MMath.TO_RADS);
                 }
             }
             else if (units == Units.Radians)
             {
-                rotsRad = jointValues;
+                rots = jointValues;
             }
             else
             {
                 throw new Exception(units + " units not allowed for this solver.");
             }
-
 
             // List of joint frames
             List<Matrix> frames = new List<Matrix>();
@@ -87,62 +93,19 @@ namespace Machina.Solvers.FK
             // Base
             frames.Add(_model.Base.BasePlane);
 
-            // Arm portion
-            List<Matrix> armFrames = ForwardKinematicsArm(rotsRad);
-            frames.AddRange(armFrames);
-
-            // Wrist portion
-            List<Matrix> wristFrames = ForwardKinematicsWrist(rotsRad.GetRange(3, 3), frames.Last());
-            frames.AddRange(wristFrames);
-
-            return frames;
-        }
-
-         /// <summary>
-        /// Compute the forward kinematics of this robot for the arm portion. 
-        /// </summary>
-        /// <param name="rots">The list of the full arm rotations in radians.</param>
-        /// <returns>List of the three transforms for the arm axes.</returns>
-        private List<Matrix> ForwardKinematicsArm(List<double> rots)
-        {
-            // List of joint frames
-            List<Matrix> frames = new List<Matrix>();
-                        
             // FRAME 1
             Plane p1 = Plane.CreateFromMatrix(_model.Base.BasePlane);
             p1.Rotate(rots[0], p1.ZAxis);
-            //Vector origin1 = plane1.PointAt(v01.X, v01.Y, v01.Z);
-            //Vector vector1 = origin1 - plane1.Origin;
-            //plane1.Translate(vector1);
             p1.Offset(v01);
             p1.Rotate(-MMath.TAU_4, p1.XAxis);
             frames.Add(p1.ToMatrix());
-
-            ////FRAME 1
-            //Plane p1 = basePlane;
-            //p1.Rotate(rots[0], p1.ZAxis);
-            //Point3d o1 = p1.PointAt(v01.X, v01.Y, v01.Z);
-            //Vector3d v1 = o1 - p1.Origin;
-            //p1.Translate(v1);
-            //p1.Rotate(-PI_2, p1.ZAxis);
-            //p1.Rotate(-PI_2, p1.YAxis);
-            //frames.Add(p1);
-
+            
             // FRAME 2
             Plane p2 = p1;
             p2.Rotate(rots[1], p2.ZAxis);
             p2.Offset(v12);
             p2.Rotate(-MMath.TAU_4, p2.ZAxis);
             frames.Add(p2.ToMatrix());
-
-            //// FRAME 2
-            //Plane p2 = p1;
-            //p2.Rotate(rots[1], p2.ZAxis);
-            //Point3d o2 = p2.PointAt(v12.X, v12.Y, v12.Z);
-            //Vector3d v2 = o2 - p2.Origin;
-            //p2.Translate(v2);
-            //p2.Rotate(PI_2, p2.ZAxis);
-            //frames.Add(p2);
 
             // FRAME 3
             Plane p3 = p2;
@@ -151,47 +114,29 @@ namespace Machina.Solvers.FK
             p3.Rotate(-MMath.TAU_4, p3.XAxis);
             frames.Add(p3.ToMatrix());
 
-            //// FRAME    
-            //Plane p3 = p2;
-            //p3.Rotate(rots[2], p3.ZAxis);
-            //Point3d o3 = p3.PointAt(v23.X, v23.Y, v23.Z);
-            //Vector3d v3 = o3 - p3.Origin;
-            //p3.Translate(v3);
-            //p3.Rotate(PI_2, p3.YAxis);
-            //p3.Rotate(PI_2, p3.ZAxis);
-            //frames.Add(p3);
-
-
-            return frames;
-        }
-
-        private List<Matrix> ForwardKinematicsWrist(List<double> rots, Matrix joint3)
-        {
-            // List of joint frames
-            List<Matrix> frames = new List<Matrix>();
-
             // FRAME 4
-            Plane p4 = Plane.CreateFromMatrix(joint3);
-            p4.Rotate(rots[0], p4.ZAxis);
+            Plane p4 = p3;
+            p4.Rotate(rots[3], p4.ZAxis);
             p4.Offset(v34);
             p4.Rotate(MMath.TAU_4, p4.XAxis);
             frames.Add(p4.ToMatrix());
 
             // FRAME 5
             Plane p5 = p4;
-            p5.Rotate(rots[1], p5.ZAxis);
+            p5.Rotate(rots[4], p5.ZAxis);
             p5.Offset(v45);
             p5.Rotate(-MMath.TAU_4, p5.XAxis);
             frames.Add(p5.ToMatrix());
 
             // FRAME 6
             Plane p6 = p5;
-            p6.Rotate(rots[2], p6.ZAxis);
+            p6.Rotate(rots[5], p6.ZAxis);
             p6.Offset(v56);
             p6.Rotate(MMath.TAU_2, p6.ZAxis);
             frames.Add(p6.ToMatrix());
 
             return frames;
         }
+
     }
 }
